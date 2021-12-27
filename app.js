62ce0e9a4cb13894d8fd6917de679497bfaacadb
Bily Hakim <bilyhakim1@gmail.com>
2021-12-27 12:47:31 +0700

    Create API Landing Page & Detail Page

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // Router Admin
 const adminRouter = require('./routes/admin');
+const apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -44,6 +45,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // Admin
 app.use('/admin', adminRouter);
+app.use('/api/v1/member', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
