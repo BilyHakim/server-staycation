cc2b5aa872b6579fd0434a8f0fc34f92179b2ff2
Bily Hakim <bilyhakim1@gmail.com>
2022-01-10 13:10:30 +0700

    Install Cors

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// Cors
+app.use(cors());
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
